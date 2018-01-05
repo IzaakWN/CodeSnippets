077010b88f2d25b2b2a314ffe4a8fc9ae3021bf2
IzaakWN <iwn_@hotmail.com>
2018-01-05 17:37:39 +0100

    extra documentation

--- C++/HelloWorld.cc
+++ C++/HelloWorld.cc
@@ -1,14 +1,23 @@
+// Author: Izaak Neutelings (March 2015)
+//
+// compile with
+//     g++ HelloWorld.cc
+// run with
+//     ./a.out
+// or do:
+//    g++ HelloWorld.cc -o HelloWorld
+//    ./HelloWorld
+
+
 #include <iostream>
 #include <math.h>
 
-double MySqrt(int x)
-{
+double MySqrt(int x){
     return sqrt(x);
 }
 
-int main()
-{
-    std::cout << "\n\tHello World!";
-    std::cout << "\n\tBtw, the sqrt of 4 is " << MySqrt(4) << ".";
-    std::cout << "\n\tI know this, because I calculated it with a function.\n\n";
+int main(){
+    std::cout << "\n  Hello World!";
+    std::cout << "\n  Btw, the sqrt of 4 is " << MySqrt(4) << ".";
+    std::cout << "\n  I know this, because I calculated it with a function.\n\n";
 }
