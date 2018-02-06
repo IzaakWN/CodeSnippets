f712564a2062ad50a3a0a9b3ff09e38547ed4ce2
IzaakWN <iwn_@hotmail.com>
2018-02-06 15:18:00 +0100

    add color

--- MadGraph/mergeLheFiles.sh
+++ MadGraph/mergeLheFiles.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-
 DIRS=`ls -d LowMassDiTau_M-*_MB-*_*`
+G="\e[37m"
+S="\e[0m"
 
 for dir in $DIRS; do
 
-  printf "\n>>> $dir\n"  
+  printf "\n>>> ${G}${dir}${S}\n"  
   OUT="$dir/$dir.lhe"
   ls $dir/*lhe > list.txt
   ./mergeLheFiles list.txt $OUT   
