3bb63ac6fb3ddeba91314f4904e9e0ff1093db9b
ineuteli <izaak.neutelings@cern.ch>
2022-05-08 16:57:34 +0200

    add compilation feature

--- python/websites/download_images.py
+++ python/websites/download_images.py
@@ -7,6 +7,8 @@
 #   https://stackoverflow.com/questions/54098316/scrape-img-src-under-div-tag-using-beautifulsoup
 # Instructions:
 #   pip install beautifulsoup4
+#   ./download_images.py -n 100 -p 20 --compile
+#   ./download_images.py -n 100 -p 20 --compile -v3
 import os, re
 import requests, contextlib
 from dateutil import parser as date_parser # for converting datestamps
@@ -18,7 +20,7 @@ rexp_img = re.compile(r"https://.*\.(je?pg|png|gif)",re.IGNORECASE)
 def format_texdoc(code,header="",verb=0):
   """Format LaTeX code into TikZ standalone."""
   tex_cls_art = r"\documentclass{article}"
-  tex_cls_tkz = r'\documentclass[tikz]{standalone}'
+  tex_cls_tkz = r'\documentclass[tikz,border=5pt]{standalone}'
   tex_pkg_tkz = r"\usepackage{tikz}"
   tex_doc_beg = r"\begin{document}"
   tex_doc_end = r"\end{document}"
@@ -36,6 +38,22 @@ def format_texdoc(code,header="",verb=0):
   if header: # add author, date, and source
     code = header+'\n'+code
   return code
+  
+
+def compile_tex(fname,convert=False,clean=True,verb=0):
+  """Compile given LaTeX file."""
+  outdir, fname = os.path.dirname(fname), os.path.basename(fname)
+  ccmd = f"cd {outdir} && pdflatex -halt-on-error {fname}; rm -v *.aux *.log"
+  if convert: # convert to png
+    pdf = fname.replace('.tex','.pdf')
+    png = fname.replace('.tex','.png')
+    ccmd += f" && convert -density 1200 -trim {pdf}[0] {png}"
+  if clean:
+    ccmd += f"; rm -v *.aux *.log"
+  if verb>=1:
+    print(f">>> compile_tex: outdir={outdir!r}, fname={fname!r}")
+    print(f">>> compile_tex: ccmd={ccmd!r}")
+  return os.system(ccmd)
   
 
 @contextlib.contextmanager
@@ -121,11 +139,11 @@ def get_codes_and_images(url:str,verb=0):
 
 def scrape_post(url,**kwargs):
   """Scrape Stack Overfow question post, and download tex code and images."""
-  dname = kwargs.get('outdir',None) or "data"
-  verb  = kwargs.get('verb',  0   )
+  dname   = kwargs.get('outdir', None) or "data"
+  verb    = kwargs.get('verb',   0   )
   #n = 3 #end value
-  os.system(f'mkdir -p {dname}') # added for automation purposes, folder can be named anything, as long as the proper name is used when saving below
-  #for i in range(n):
+  os.makedirs(dname,exist_ok=True)
+  texfiles = [ ] # for compilation
   with get_codes_and_images(url,verb=verb) as (texdocs, texsnips, imgs):
     print(f">>> FOUND {len(texdocs)} tex files, {len(texsnips)} snippets, and {len(imgs)} images")
     if len(texdocs)==0:
@@ -137,35 +155,50 @@ def scrape_post(url,**kwargs):
         print(f">>> DOWNLOAD {imgurl}")
       match = rexp_img.match(imgurl)
       ext = match.group(1).lower()
-      with open(f'{dname}/image_{i}.{ext}','wb') as file:
+      fname = f'{dname}/image_{i}.{ext}'
+      with open(fname,'wb') as file:
         file.write(requests.get(imgurl).content)
     
-    # WRITE TEX CODE
+    # WRITE TEX DOCUMENT
     for i, code in enumerate(texdocs,1):
-      with open(f'{dname}/code_{i}.tex','w') as file:
+      fname = f'{dname}/code_{i}.tex'
+      with open(fname,'w') as file:
         file.write(code)
+      if r'\includegraphics' not in code:
+        texfiles.append(fname)
     
-    # WRITE TEX CODE
+    # WRITE TEX CODE SNIPPET
     for i, code in enumerate(texsnips,1):
-      with open(f'{dname}/code_snippet_{i}.tex','w') as file:
+      fname = f'{dname}/code_snippet_{i}.tex'
+      with open(fname,'w') as file:
         file.write(code)
-    
+      if r'\includegraphics' not in code:
+        texfiles.append(fname)
+  
+  return texfiles
+  
 
 def main(args):
-  verb   = args.verb  # verbosity level for debugging
-  ipost  =   1        # index of current post
-  nposts = args.posts # maximum number of posts to download
-  npages = args.pages # maximum number of pages with search results
+  
+  # SETTINGS
+  compile = args.compile # compile LaTeX
+  verb    = args.verb  # verbosity level for debugging
+  nposts  = args.posts # maximum number of posts to download
+  npages  = args.pages # maximum number of pages with search results
   searchpage = "https://tex.stackexchange.com/search?tab=votes&q=user%3a2552%20%5btikz-pgf%5d"
   #posts = ["https://tex.stackexchange.com/questions/9559/drawing-on-an-image-with-tikz/",]
-  blacklist = ['18389','42401'] # ignore these posts
+  blacklist = ['58878','18389','42401'] # ignore these posts
+  
+  # DOWNLOAD CODE & IMAGES
+  ipost    = 1   # index of current post
+  texfiles = [ ] # for compilation
   for ipage in range(1,npages+1):
     if ipost>nposts: break # limit number of posts
     pageurl = searchpage+(f"&page={ipage}" if ipage>1 else "")
     print(">>>"+"\n>>> "*2)
     print(f">>> # PAGE {ipage}: {pageurl}")
     with get_SO_posts(pageurl,verb=verb) as posts:
-      if verb+3>=2:
+      if verb>=2:
         print(f">>> FOUND posts:")
         for url in posts: print(f">>>  {url}")
       for url in posts:
@@ -186,24 +219,40 @@ def main(args):
           print(f">>> {url}")
           #print(f">>> ")
           outdir = f"data/question_{ipost:03d}_{qname}" #_{qid}
-          scrape_post(url,outdir=outdir,verb=verb)
+          texfiles += scrape_post(url,outdir=outdir,verb=verb)
           ipost += 1
           print(f">>> ")
         else:
           print(f">>> Did not recognize URL for StackOverflow question: {url}")
-    print(">>>"+"\n>>> "*2)
-    
+    print(">>>"+"\n>>> "*2)  
+  
+  # COMPILE TEX FILES
+  print (texfiles)
+  if compile:
+    print(f">>> Compile {len(texfiles)} LaTeX files...")
+    failed = [ ]
+    for fname in texfiles:
+      retcode = compile_tex(fname,convert=True,verb=verb)
+      if retcode>0: # failed?
+        failed.append((fname,retcode))
+    print(">>> Done compiling!")
+    if failed:
+      print(">>> Compilation/Conversion of the following files failed (return code > 0):")
+      for fname, retcode in failed:
+        print(f">>>  {retcode:4d}: {fname}")
+      
+
 
 # QUICK PLOTTING SCRIPT
 if __name__ == '__main__':
   from argparse import ArgumentParser
   description = """Download images from web page."""
   parser = ArgumentParser(prog="download_images.py",description=description,epilog="Good luck!")
-  parser.add_argument('file', help="final (hadd'ed) ROOT file")
-  parser.add_argument('-n','--posts', type=int,default=10,help="maximum number of posts, default=%(default)s")
-  parser.add_argument('-p','--pages', type=int,default=1,help="maximum number of pages, default=%(default)s")
-  parser.add_argument('-v','--verb',  dest='verbosity',type=int,nargs='?',const=1,default=0,
-                                      help="set verbosity")
+  parser.add_argument('-n','--posts',  type=int,default=10,help="maximum number of posts, default=%(default)s")
+  parser.add_argument('-p','--pages',  type=int,default=1,help="maximum number of pages, default=%(default)s")
+  parser.add_argument('-c','--compile',action='store_true',help="compile LaTeX")
+  parser.add_argument('-v','--verb',   type=int,nargs='?',const=1,default=0,
+                                       help="set verbosity")
   args = parser.parse_args()
   main(args)
   
